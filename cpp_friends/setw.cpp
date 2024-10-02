4843686df311bd1f1624c97c840edbaac73c5c98
meowka <115697567+1meowka@users.noreply.github.com>
2024-10-02 11:08:53 +0500

    Update setw.cpp

--- cpp_friends/setw.cpp
+++ cpp_friends/setw.cpp
@@ -1,18 +1,28 @@
-#include <string>
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-int main ()
-{
-  string one = "Один", two = "Два", eight = "Восемь";
-  cout << setw (10) << one <<endl;
-  cout << setw (10) << two <<endl;
-  cout << setw (10) << eight <<endl;
-  return 0;
+// Функция для печати информации о друге
+void printFriend(const Friend &f) {
+    cout << left << setw(15) << f.lastName 
+         << setw(15) << f.firstName 
+         << setw(15) << f.middleName 
+         << setw(2) << setfill('0') << f.day << "." << setw(2) << setfill('0') << f.month << "." << f.year 
+         << "   " << setw(20) << f.address 
+         << setw(15) << f.phone << endl;
 }
 
+// Функция для печати всех друзей
+void printAllFriends(const list<Friend> &friends) {
+    if (friends.empty()) { 
+        cout << "[INFORMATION] --- Список друзей пуст.\n";
+        return;
+    }
+    
+    cout << left << setw(15) << "Фамилия" 
+         << setw(15) << "Имя" 
+         << setw(15) << "Отчество"
+         << "Дата рождения   " << setw(20) << "Адрес" 
+         << setw(15) << "Телефон" << endl;
+    cout << string(80, '-') << endl;
 
-//Command for c++:               g++ friends -o different_name
-//                               ./different_name
+    for (const auto &f : friends) {
+        printFriend(f);
+    }
+}
