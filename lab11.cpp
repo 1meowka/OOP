a77585598edb78453bf7a1ff39d911e0f625a4bd
meowka <115697567+1meowka@users.noreply.github.com>
2024-10-16 11:01:53 +0500

    Update lab11.cpp

--- lab11.cpp
+++ lab11.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-const int MAX_FRIENDS = 100; // Максимальное количество друзей
-const int COLUMN_WIDTH = 15; // Ширина колонки
+const int MAX_FRIENDS = 100;
+const int COLUMN_WIDTH = 15;
 
 struct Friend {
     string lastName;
@@ -19,7 +19,7 @@ struct Friend {
 };
 
 Friend friends[MAX_FRIENDS];
-int friend_count = 0; // Текущее количество друзей
+int friend_count = 0;
 
 int get_friend_count() {
     return friend_count;
@@ -30,35 +30,33 @@ void add_friend(const Friend& new_friend) {
         friends[friend_count] = new_friend;
         friend_count++;
     } else {
-        cout << "Невозможно добавить больше друзей.\n";
+        cout << "You can't add a friend, because the limit is 100 friends\n";
     }
 }
 
 void delete_friend(int index) {
     if (index >= 0 && index < get_friend_count()) {
         for (int i = index; i < friend_count - 1; ++i) {
-            friends[i] = friends[i + 1]; 
+            friends[i] = friends[i + 1];
         }
-        friend_count--; 
+        friend_count--;
     } else {
-        cout << "Неправильный ввод.\n";
+        cout << "Error: Invalid index\n";
     }
 }
 
-// Функция для печати таблицы друзей
 void print_friends() {
     if (get_friend_count() == 0) {
-        cout << "Список друзей пуст.\n";
+        cout << "The friends list is empty\n";
         return;
     }
 
-    // Заголовок таблицы
-    cout << "| " << setw(COLUMN_WIDTH) << left << "fam"
-         << "| " << setw(COLUMN_WIDTH) << left << "name"
-         << "| " << setw(COLUMN_WIDTH) << left << "middlename"
-         << "| " << setw(COLUMN_WIDTH) << left << "bithday"
-         << "| " << setw(COLUMN_WIDTH) << left << "sity"
-         << "| " << setw(COLUMN_WIDTH) << left << "number" << " |" << endl;
+    cout << "| " << setw(COLUMN_WIDTH) << left << "Lastname"
+         << "| " << setw(COLUMN_WIDTH) << left << "Name"
+         << "| " << setw(COLUMN_WIDTH) << left << "Middle name"
+         << "| " << setw(COLUMN_WIDTH) << left << "Date of birth"
+         << "| " << setw(COLUMN_WIDTH) << left << "City"
+         << "| " << setw(COLUMN_WIDTH) << left << "Phone number" << " |" << endl;
     cout << string(100, '-') << endl;
 
     for (int i = 0; i < get_friend_count(); ++i) {
@@ -66,37 +64,38 @@ void print_friends() {
              << "| " << setw(COLUMN_WIDTH) << left << friends[i].firstName
              << "| " << setw(COLUMN_WIDTH) << left << friends[i].middleName
              << "| " << setw(COLUMN_WIDTH) << left << friends[i].day << "."
-             << (friends[i].month < 10 ? "0" : "") << friends[i].month << "." << friends[i].year
+             << (friends[i].month < 10 ? "0" : "") << friends[i].month << "."
+             << friends[i].year
              << "| " << setw(COLUMN_WIDTH) << left << friends[i].address
              << "| " << setw(COLUMN_WIDTH) << left << friends[i].phone << " |" << endl;
         cout << string(100, '-') << endl;
     }
 }
 
-// Функция для поиска друзей по месяцу рождения
-void find_friends_by_month(int month) {
-    bool found = false;
+void print_friends_by_month(int month) {
+    if (month < 1 || month > 12) {
+        cout << "Invalid month. Please enter a number between 1 and 12.\n";
+        return;
+    }
 
-    // Заголовок таблицы (выводим только если есть совпадения)
+    bool found = false;
+    cout << "| " << setw(COLUMN_WIDTH) << left << "Lastname"
+         << "| " << setw(COLUMN_WIDTH) << left << "Name"
+         << "| " << setw(COLUMN_WIDTH) << left << "Middle name"
+         << "| " << setw(COLUMN_WIDTH) << left << "Date of birth"
+         << "| " << setw(COLUMN_WIDTH) << left << "City"
+         << "| " << setw(COLUMN_WIDTH) << left << "Phone number" << " |" << endl;
+    cout << string(100, '-') << endl;
+
     for (int i = 0; i < get_friend_count(); ++i) {
         if (friends[i].month == month) {
-            if (!found) {
-                cout << "| " << setw(COLUMN_WIDTH) << left << "fam"
-                     << "| " << setw(COLUMN_WIDTH) << left << "name"
-                     << "| " << setw(COLUMN_WIDTH) << left << "middlename"
-                     << "| " << setw(COLUMN_WIDTH) << left << "bithday"
-                     << "| " << setw(COLUMN_WIDTH) << left << "sity"
-                     << "| " << setw(COLUMN_WIDTH) << left << "number" << " |" << endl;
-                cout << string(100, '-') << endl;
-                found = true;
-            }
-
-            // Выводим только тех друзей, у кого месяц рождения совпадает
+            found = true;
             cout << "| " << setw(COLUMN_WIDTH) << left << friends[i].lastName
                  << "| " << setw(COLUMN_WIDTH) << left << friends[i].firstName
                  << "| " << setw(COLUMN_WIDTH) << left << friends[i].middleName
                  << "| " << setw(COLUMN_WIDTH) << left << friends[i].day << "."
-                 << (friends[i].month < 10 ? "0" : "") << friends[i].month << "." << friends[i].year
+                 << (friends[i].month < 10 ? "0" : "") << friends[i].month << "."
+                 << friends[i].year
                  << "| " << setw(COLUMN_WIDTH) << left << friends[i].address
                  << "| " << setw(COLUMN_WIDTH) << left << friends[i].phone << " |" << endl;
             cout << string(100, '-') << endl;
@@ -104,7 +103,7 @@ void find_friends_by_month(int month) {
     }
 
     if (!found) {
-        cout << "Друзей с месяцем рождения " << month << " не найдено.\n";
+        cout << "No friends found for the specified month.\n";
     }
 }
 
@@ -112,54 +111,54 @@ int main() {
     int choice;
 
     while (true) {
-        cout << "1. Добавить друга\n";
-        cout << "2. Удалить друга\n";
-        cout << "3. Вывод всех друзей\n";
-        cout << "4. Поиск по месяцу рождения\n";
-        cout << "5. Выход\n";
-        cout << "Введите свой выбор: ";
+        cout << "1. Add a friend\n";
+        cout << "2. Delete a friend\n";
+        cout << "3. Print all friends\n";
+        cout << "4. Search by month of birth\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
         cin >> choice;
 
         if (choice == 1) {
             Friend new_friend;
-            cout << "Введите фамилию: ";
+            cout << "Enter lastname: ";
             cin.ignore();
             getline(cin, new_friend.lastName);
-            cout << "Введите имя: ";
+            cout << "Enter name: ";
             getline(cin, new_friend.firstName);
-            cout << "Введите отчество: ";
+            cout << "Enter middle name: ";
             getline(cin, new_friend.middleName);
-            cout << "Введите день рождения: ";
+            cout << "Enter DAY of birth: ";
             cin >> new_friend.day;
-            cout << "Введите месяц рождения: ";
+            cout << "Enter MONTH of birth: ";
             cin >> new_friend.month;
-            cout << "Введите год рождения: ";
+            cout << "Enter YEAR of birth: ";
             cin >> new_friend.year;
             cin.ignore();
-            cout << "Введите город: ";
+            cout << "Enter city: ";
             getline(cin, new_friend.address);
-            cout << "Введите телефон: ";
+            cout << "Enter phone number: ";
             getline(cin, new_friend.phone);
 
             add_friend(new_friend);
-            cout << "Друг добавлен!\n\n";
+            cout << "Friend added!\n\n";
 
         } else if (choice == 2) {
             int index;
-            cout << "Для удаления введите номер друга: ";
+            cout << "Enter index of the friend to delete: ";
             cin >> index;
             delete_friend(index - 1);
         } else if (choice == 3) {
             print_friends();
         } else if (choice == 4) {
             int month;
-            cout << "Введите месяц (1-12): ";
+            cout << "Enter MONTH (1-12): ";
             cin >> month;
-            find_friends_by_month(month);
+            print_friends_by_month(month);
         } else if (choice == 5) {
             break;
         } else {
-            cout << "Неправильный ввод, пожалуйста повторите.\n";
+            cout << "Invalid choice. Please try again.\n";
         }
     }
 
